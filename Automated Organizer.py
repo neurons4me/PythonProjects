7803b6e82091b0cb37130bd968532856d367fbaa
neurons4me <adammcdaniel1@gmail.com>
2018-03-30 13:39:23 -0700

    Add files via upload

--- Automated Organizer.py
+++ Automated Organizer.py
@@ -1,41 +1,48 @@
-#intialization section; todo: pull all changable info from a text config file
-
-import shutil, os, datetime
-watchdir = os.path.join('C:\\','Move testing','Test Source Folder') # folder where you drop the shit you want organized
-dirlist = ["DBCA", "Category 1"], ["ABCD", "Category 2"] # a library of project codes and destination folder names
-filelist = os.listdir(watchdir) #captures a list of the files in the watch dirrectory in order to itterate over them
+#INTIALIZATION
+import shutil, os, datetime, configparser
 todaysdate = str(datetime.date.today())
-defaultfolder = "Test Destination Folder"
-# to make adding cetegories easier have a check durrin intialization to make sure each item in dirlist have a corresponding folder created already or create it. that way all you need to do to add new types is edit the config file
 
 
-for startfilename in filelist: #itterates over the list of files in the watch dirrectory, sorts them by project code, renames with a date appended, and moves them to the appropriate folder
-    
-    fileext = os.path.splitext(startfilename)
-    endfilename = fileext[0] + "_" + todaysdate + fileext[1]
-    justname = fileext[0] 
+#IMPORT FROM CONFIG.INI
+parser = configparser.ConfigParser()
+parser.read('config.ini')
+watchdir = str(parser.get('dir','watchdir'))
+defaultfolder = str(parser.get('dir','defaultfolder'))
+parentdir = str(parser.get('dir','parentdir'))
+dirlist = {}
+for code, folder in parser['projectcodes'].items():
+    dirlist[code] = folder
 
-    if dirlist[0][0] in justname : #this is not an exact match situation... is that going to be a problem?
-        destfolder = dirlist[0][1]
-    elif dirlist[1][0] in justname :
-        destfolder = dirlist[1][1]
-    else:
-        destfolder = defaultfolder
+#MAIN SORT LOOP
+filelist = os.listdir(watchdir) 
+for startfilename in filelist:    
+    fileext = os.path.splitext(startfilename) 
+    endfilename = fileext[0] + "_" + todaysdate + fileext[1] 
+    justname = fileext[0]
+    justnamelower = justname.lower() 
 
-    dstpath = str(os.path.join('C:\\', 'Move testing',destfolder))
-    src = os.path.join(watchdir ,startfilename)
+    #CODE PARSING AND SORTING LOOP
+    destfolder = defaultfolder
+    for projcode in dirlist.keys():
+        if projcode in justnamelower:
+            destfolder = dirlist.get(projcode)        
+
+    #CREATE FOLDER IF NEEDED
+    dstpath = parentdir + destfolder
+    if os.path.isdir(dstpath) != True:
+        os.makedirs(dstpath)
+        
     dstlist = os.listdir(dstpath)
     
 
-    # makes sure to append a number to end to prevent overwriting if final name will be the same as a file in the destination folder; I think the the If is redundant but it works this way
+    #OVERWRITE PREVENTION LOOP
     if any(endfilename in s for s in dstlist):
         dupnameinc = 0
         while endfilename in dstlist:
             dupnameinc = dupnameinc + 1
             endfilename = fileext[0] + "_" + todaysdate + "_" + str(dupnameinc) + fileext[1]
 
-    # finalizes the destination of the move            
-    dst = os.path.join('C:\\', 'Move testing',destfolder,endfilename)
-
-    #performs the move and rename
+    #MOVE       
+    src = os.path.join(watchdir ,startfilename)
+    dst = os.path.join(parentdir,destfolder,endfilename)
     shutil.move(src,dst)
